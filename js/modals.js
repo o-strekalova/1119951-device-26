c484d17224f256dfc977be11703e482d5c1ce0c8
o-strekalova <strekalova.olg@yandex.ru>
2019-10-29 21:33:42 +0300

    Исправления

--- js/modals.js
+++ js/modals.js
@@ -66,3 +66,27 @@
       }
     }
   });
+
+  var mapLink = document.querySelector(".open-map");
+
+  var mapPopup = document.querySelector(".modal-map");
+  var mapClose = mapPopup.querySelector(".close-button");
+
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
+
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show")) {
+        evt.preventDefault();
+        mapPopup.classList.remove("modal-show");
+      }
+    }
+  });
